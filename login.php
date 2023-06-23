de470609c09ae97ca0514cdcf3f0765603de28f5
Daniel Garrocho <danmgap@gmail.com>
2023-06-22 22:01:10 -0300

    22/06
    adicionei um monte de coisa bagunçada aqui, como autenticador

--- login.php
+++ login.php
@@ -8,12 +8,12 @@
     //Variavel que verifica se a autenticação foi realizada
     $usuario_autenticado = false;
     $usuario_id = null;
-    $usuario_perfil_id = null;
+    $usuario_senha = null;
     $usuario_email =null;
 
     $valida_usuario = md5($_POST['usuario']);
     $valida_senha = md5($_POST['senha']);
-    $sql = "SELECT * FROM tb_perfil WHERE nome_perfil = '$valida_usuario' AND senha_perfil = '$valida_senha'";
+    $sql = "SELECT * FROM tb_usuario WHERE nome_usuario = '$valida_usuario' AND senha_usuario = '$valida_senha'";
     /*if( isset($_POST['usuario']) && !empty($_POST['usuario']) || $_POST['usuario']==='0'){
         $sql = $sql . " and senha = '$senha'";
         
@@ -24,11 +24,10 @@
         $resultado = mysqli_fetch_assoc($result);
         if(isset($resultado)){
             $_SESSION['autenticado']='SIM';
-            $_SESSION['id'] = $resultado['id_perfil'];
+            $_SESSION['id'] = $resultado['id_usuario'];
             $_SESSION['nome'] = $_POST['usuario'];
-            $_SESSION['email'] = $resultado['email_perfil'];
+            $_SESSION['email'] = $resultado['email_usuario'];
             $_SESSION['senha'] = $_POST['senha'];
-            $_SESSION['num_tweet'] = $num_tweets;
             header('Location: dataetweet.php');
             header('Location: home.php');
         }
