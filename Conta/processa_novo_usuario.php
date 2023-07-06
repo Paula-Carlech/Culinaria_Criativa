84b34061fcb1e6b3b7580233e7f6ee2d4bc66ae2
Daniel Garrocho <danmgap@gmail.com>
2023-07-06 17:10:43 -0300

    mais coisas
    as

--- Conta/processa_novo_usuario.php
+++ Conta/processa_novo_usuario.php
@@ -1,10 +1,19 @@
 <?php
     include('../Conectar/conectar.php');
+    session_start();
+    $nusuari_pnome = $_POST['USERNpnome'];
+    $nusuari_unome = $_POST['USERNunome'];
+    $nusuari_email = $_POST['USERNani'];
+    $nusuari_senha = $_POST['USERNemail'];
+    $nusuari_ani = $_POST['USERNpwd'];
 
-    $nusuari_pnome = $_SESSION['USERNpnome'];
-    $nusuari_unome = $_SESSION['USERNunome'];
-    $nusuari_email = $_SESSION['USERNani'];
-    $nusuari_senha = $_SESSION['USERNemail'];
-    $nusuari_ani = $_SESSION['USERNpwd'];
-    
+    $hash_nusuari_pnome = password_hash($nusuari_pnome, PASSWORD_DEFAULT);
+    $hash_nusuari_unome = password_hash($nusuari_unome, PASSWORD_DEFAULT);
+    $hash_nusuari_email = password_hash($nusuari_email, PASSWORD_DEFAULT);
+    $hash_nusuari_senha = password_hash($nusuari_senha, PASSWORD_DEFAULT);
+    $hash_nusuari_ani = password_hash($nusuari_ani, PASSWORD_DEFAULT);
+
+    $sql_nuser = "INSERT INTO cc_usuario(Primeiro_Nome_Usuario,Ultimo_Nome_Usuario,Senha_Usuario,Email_Usuario,Aniversario_Usuario) VALUES ($hash_nusuari_pnome,$hash_nusuari_unome,$hash_nusuari_senha,$hash_nusuari_email,$hash_nusuari_ani)";
+    $resultado_insert_nuser = mysqli_query($conn,$sql_nuser);
+
 ?>
