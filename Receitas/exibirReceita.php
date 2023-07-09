7786f8b7d88cc1a687d81c9041d4078fd5c29eac
Paula-Carlech <89168944+Paula-Carlech@users.noreply.github.com>
2023-07-09 11:25:19 -0300

    Update exibirReceita.php

--- Receitas/exibirReceita.php
+++ Receitas/exibirReceita.php
@@ -1,8 +1,5 @@
-<?php 
-    include();
-?>
-
 <?php
+
 $servername = "localhost";
 $username = "seu_usuario";
 $password = "sua_senha";
@@ -13,20 +10,20 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Falha na conexão com o banco de dados: " . $conn->connect_error);
 }
-?>
 
-<?php
 $sql = "SELECT nome, ingredientes, modo_preparo FROM receitas";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
+    $modalId = 1; // ID inicial do modal
+
     while ($row = $result->fetch_assoc()) {
         $nome = $row["nome"];
         $ingredientes = $row["ingredientes"];
         $modoPreparo = $row["modo_preparo"];
 
         // Exiba as informações da receita
-        echo '<div class="modal fade" id="modal-1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">';
+        echo '<div class="modal fade" id="modal-' . $modalId . '" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">';
         echo '  <div class="modal-dialog">';
         echo '    <div class="modal-content">';
         echo '      <div class="modal-header">';
@@ -34,7 +31,7 @@ if ($result->num_rows > 0) {
         echo '        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>';
         echo '      </div>';
         echo '      <div class="modal-body m-auto text-justify">';
-        echo '        <img class="rounded img-modal w-100" src="assets/images/foto-salgada.png" alt="Foto da receita.">';
+        echo '        <img class="rounded img-modal w-100" src="assets/images/foto-' . $modalId . '.png" alt="Foto da receita.">';
         echo '        <h3>Ingredientes</h3>';
         echo '        <p class="texto">' . $ingredientes . '</p>';
         echo '        <h3>Modo de Preparo</h3>';
@@ -46,6 +43,8 @@ if ($result->num_rows > 0) {
         echo '    </div>';
         echo '  </div>';
         echo '</div>';
+
+        $modalId++; // Incrementa o ID do modal
     }
 } else {
     echo "Nenhuma receita encontrada.";
